5dfa64839a940c2b5cd907c3af586261f1fae0fa
Arsen <84477625+Arsentyio@users.noreply.github.com>
2022-03-23 18:39:50 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,10 @@
+TOKEN = '5108054005:AAGv-KU64KkgdCPzolJUy0M8wyWZZGm3iAc'
+
+keys = {
+    'доллар': 'USD',
+    'рубль': 'RUB',
+    'евро': 'EUR',
+    'йена': 'JPY',
+    'британский_фунт': 'GBP',
+    'турецкая_лира': 'TRY',
+}
